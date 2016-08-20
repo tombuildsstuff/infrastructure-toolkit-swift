b19b4889bd3a3097c97be43442d749d5f10ff23d
tombuildsstuff <git@ibuildstuff.co.uk>
2016-08-20 13:41:03 +0100

    Making the MetaData public

--- src/InfrastructureToolkit/Entities/ServiceStatusResultMetaData.swift
+++ src/InfrastructureToolkit/Entities/ServiceStatusResultMetaData.swift
@@ -1,4 +1,9 @@
 public struct ServiceStatusResultMetaData {
     public let summary : String
     public let properties : [String : String]?
+
+    public init(_ summary: String, _ properties: [String: String]?) {
+      self.summary = summary
+      self.properties = properties
+    }
 }
